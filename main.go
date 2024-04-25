82c4cab7dc3da79bb643bfcdaea3360cd8af1e01
Manoel Fernando C. Bentes <mfcbentes.stm@gmail.com>
2024-04-24 22:11:24 -0300

    Incluir Scan para entrada de dados

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	tempK := 120.00
+	var tempK float64
 	var tempC float64
+
+	fmt.Print("Digite a temperatura em Kelvin: ")
+	fmt.Scanln(&tempK)
 
 	tempC = tempK - 273.15
 
-	fmt.Printf("%.2f Kelvin equivalem a %.2f graus Celsius \n", tempK, tempC)
+	fmt.Printf("%.2f Kelvin equivalem a %.2f graus Celsius\n", tempK, tempC)
 }
